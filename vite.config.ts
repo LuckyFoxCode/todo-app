9a43a3516c4fc13e6e7b3ca03954598545ac8a92
LuckyFoxCode <sergei181989@gmail.com>
2024-07-12 11:45:13 +0300

    🔧 Update vite.config.js
    Changed baseURL in vite.config.js file.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './todo-app',
+  base: '/todo-app',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
